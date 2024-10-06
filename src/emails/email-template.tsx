cd59803f0ff59c219824a72b999307fe16e1b81c
Xavier Galdeano <xavier.galdeano@dinotech.com>
2024-10-06 22:32:20 +0200

    resend to me

--- src/emails/email-template.tsx
+++ src/emails/email-template.tsx
@@ -1,15 +1,19 @@
 import * as React from 'react';
 
 interface EmailTemplateProps {
-	firstName: string;
+	firstName?: string | null;
+	lastName?: string | null;
+	phoneNumber?: string | null;
+	email?: string | null;
 }
 
-export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({ firstName, lastName, phoneNumber }) => (
+export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({ firstName, lastName, phoneNumber, email }) => (
 	<div>
-		<h1>Dolce Gabanna contact!</h1>
-		<span>Firstname: {firstName}</span>
-		<span>Lastname: {lastName}</span>
-		<span>Phone number: {phoneNumber}</span>
+		<h1>Dolce Gabanna contact</h1>
+		<p>Firstname: {firstName}</p>
+		<p>Lastname: {lastName}</p>
+		<p>Email: {email}</p>
+		<p>Phone number: {phoneNumber}</p>
 	</div>
 );
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,22 @@ import { EmailTemplate } from './emails/email-template';
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		const resend = new Resend('re_R5zCxn2E_NfkyuvzRNtiLBv6n15MKYBvK' /* env.RESEND_API_KEY */);
+		const resend = new Resend('re_FvZbuc2B_7dRwM2UzDZR7x6Z2xsTVH2HL' /* env.RESEND_API_KEY */);
 
-		const { firstName, lastName, phoneNumber } = request.body;
+		const url = new URL(request.url);
 
 		const data = await resend.emails.send({
 			from: 'onboarding@resend.dev',
-			to: 'sales@sierrablancaestates.com',
+			to: 'xavier@galdeano.io',
 			subject: 'Dolce Gabanna contact!',
-			react: <EmailTemplate firstName={firstName} lastName={lastName} phoneNumber={phoneNumber} />,
+			react: (
+				<EmailTemplate
+					firstName={url.searchParams.get('firstName')}
+					lastName={url.searchParams.get('lastName')}
+					phoneNumber={url.searchParams.get('phoneNumber')}
+					email={url.searchParams.get('email')}
+				/>
+			),
 		});
 
 		return new Response(JSON.stringify(data), {
